4139171f7eee123adb96e68e66e9e593296e6579
Dominic Dagradi <dominic@chain.com>
2017-04-07 16:52:35 -0700

    render tokens and certs in separate tables

--- dashboard/src/features/accessControl/components/AccessControlList.jsx
+++ dashboard/src/features/accessControl/components/AccessControlList.jsx
@@ -1,14 +1,57 @@
+import React from 'react'
 import AccessGrantListItem from './AccessGrantListItem'
-import { BaseList, TableList } from 'features/shared/components'
+import { connect } from 'react-redux'
+import { TableList, PageTitle, PageContent } from 'features/shared/components'
 
-const type = 'accessControl'
+// export default BaseList.connect(
+//   BaseList.mapStateToProps(type, AccessGrantListItem, {
+//     skipQuery: true,
+//     label: 'Access control',
+//     wrapperComponent: TableList,
+//     wrapperProps: {
+//       titles: ['ID', 'Grant']
+//     }
+//   }),
+//   BaseList.mapDispatchToProps(type)
+// )
 
-export default BaseList.connect(
-  BaseList.mapStateToProps(type, AccessGrantListItem, {
-    wrapperComponent: TableList,
-    wrapperProps: {
-      titles: ['ID', 'Grant']
+class AccessControlList extends React.Component {
+  render() {
+    const tableListProps = {
+      titles: ['ID', 'Policy']
     }
-  }),
-  BaseList.mapDispatchToProps(type)
-)
+
+    const tokenList = <TableList {...tableListProps}>
+      {this.props.tokens.map(item => <AccessGrantListItem item={item} />)}
+    </TableList>
+
+    const certList = <TableList {...tableListProps}>
+      {this.props.certs.map(item => <AccessGrantListItem item={item} />)}
+    </TableList>
+
+    return (<div>
+      <PageTitle title='Access Control' />
+
+      <PageContent>
+        {tokenList}
+        {certList}
+      </PageContent>
+    </div>)
+  }
+}
+
+const mapStateToProps = (state) => {
+  const items = Object.values(state.accessControl.items)
+  return {
+    tokens: items.filter(item => item.type == 'access_token'),
+    certs: items.filter(item => item.type == 'x509'),
+  }
+}
+
+const mapDispatchToProps = ( ) => ({})
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AccessControlList)
