716d63409721c5f44a034c29cb51ad5d53ea895c
Dominic Dagradi <dominic@chain.com>
2017-04-06 17:49:49 -0700

    add placeholder ACL node interface

---
+++ sdk/node/src/api/accessControl.js
@@ -0,0 +1,16 @@
+const shared = require('../shared')
+
+const accessControl = (client) => ({
+  create: (params, cb) =>
+    shared.create(client, '/acl-grant', params, {skipArray: true}, cb),
+
+  delete: (params, cb) => shared.tryCallback(
+    client.request('/acl-revoke', params),
+    cb
+  ),
+
+  list: (params, cb) =>
+    shared.query(client, 'accessTokens', '/list-acl-grants', params, {cb}),
+})
+
+module.exports = accessControl
